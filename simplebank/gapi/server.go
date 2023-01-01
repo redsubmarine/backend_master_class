9618c3df5c9f8e777a87c047869ce44e76adc726
red <redsubmarine@yahoo.com>
2023-01-01 21:07:31 +0900

    add gRPC server

---
+++ simplebank/gapi/server.go
@@ -0,0 +1,35 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/redsubmarine/simplebank/db/sqlc"
+	"github.com/redsubmarine/simplebank/pb"
+	"github.com/redsubmarine/simplebank/token"
+	"github.com/redsubmarine/simplebank/util"
+)
+
+// Server serves gRPC requests for our banking service.
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
--- simplebank/main.go
+++ simplebank/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	_ "github.com/lib/pq"
 	"github.com/redsubmarine/simplebank/api"
 	db "github.com/redsubmarine/simplebank/db/sqlc"
+	"github.com/redsubmarine/simplebank/gapi"
+	"github.com/redsubmarine/simplebank/pb"
 	"github.com/redsubmarine/simplebank/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -22,11 +27,37 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server: ", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPc server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPc server")
+	}
+
+}
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server: ", err)
 	}
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
