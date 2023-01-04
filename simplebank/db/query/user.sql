96826480e83ae311d1fe6c3cb2f33e5fe02638c8
red <redsubmarine@yahoo.com>
2023-01-04 16:57:30 +0900

    partial update db record with sqlc nullable params

--- simplebank/db/query/user.sql
+++ simplebank/db/query/user.sql
@@ -12,3 +12,34 @@ RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users
 WHERE username = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE users 
+SET
+  hashed_password = COALESCE(sqlc.narg(hashed_password), hashed_password),
+  full_name = COALESCE(sqlc.narg(full_name), full_name),
+  email = COALESCE(sqlc.narg(email), email)
+WHERE 
+  username = sqlc.arg(username)
+RETURNING *;
+
+/*
+-- name: UpdateUser :one
+UPDATE users 
+SET
+    hashed_password = CASE
+      WHEN sqlc.arg(set_hashed_password)::boolean = TRUE THEN sqlc.arg(hashed_password)
+      ELSE hashed_password
+    END,
+    full_name = CASE
+      WHEN @set_full_name::boolean = TRUE THEN @full_name
+      ELSE full_name
+    END,
+    email = CASE
+      WHEN @set_email::boolean = TRUE THEN @email
+      ELSE email
+    END
+WHERE 
+  username = @username
+RETURNING *;
+*/
